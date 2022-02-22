3c5277c6000d6901ea619250f14ff7eb6f5ed105
CTFrancato <cfrancato@uol.com.br>
2022-02-21 22:40:10 -0300

    JPA repository dependency injection database seeding

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -15,7 +15,7 @@ public class User  implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String nome;
+	private String name;
 	private String email;
 	private String phone;
 	private String password;
@@ -23,10 +23,10 @@ public class User  implements Serializable{
 	public User() {
 }
 
-	public User(long id, String nome, String email, String phone, String password) {
+	public User(long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
-		this.nome = nome;
+		this.name = name;
 		this.email = email;
 		this.phone = phone;
 		this.password = password;
@@ -40,12 +40,12 @@ public class User  implements Serializable{
 		this.id = id;
 	}
 
-	public String getNome() {
-		return nome;
+	public String getName() {
+		return name;
 	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	public String getEmail() {
@@ -76,7 +76,7 @@ public class User  implements Serializable{
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
 
@@ -89,12 +89,18 @@ public class User  implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		if (id != other.id)
+		if(id == null) {
+			if(other.id != null)
+				return false;
+		}else if (!id.equals(other.id))
 			return false;
 		return true;
+		}
+
+
 	}
 
 
 
 	
-}
+
